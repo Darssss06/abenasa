71626a7f3fa279356b405b79e53a22e013b9b5ef
ACT-STUDENT <ACT-STUDENT@LAB4-PC44>
2025-08-07 18:42:18 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace abenasa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(""); 
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
